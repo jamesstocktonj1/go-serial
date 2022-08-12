9d7093199062590858d0473aa039fa8fb528ab25
js3g20 <js3g20@soton.ac.uk>
2022-08-12 21:04:13 +0100

    Create Error

--- output.go
+++ output.go
@@ -14,5 +14,5 @@ type SerialStream struct {
 
 
 func NewSerialStream(portName string, portColor color.Attribute) *SerialStream  {
-	return &SerialStream { portColor, portName }
+	return &SerialStream { portColor, "COM_X" }
 }
