08bdd8747112db74f51f56522e6241ee7edf00fc
js3g20 <js3g20@soton.ac.uk>
2022-08-12 23:18:58 +0100

    Update Output

--- output_test.go
+++ output_test.go
@@ -20,3 +20,44 @@ func TestNewSerialStream(t *testing.T)  {
 		t.Errorf("expected serial name %s but got %s", testName, testStream.serialName)
 	}
 }
+
+
+func TestFormatSerialName(t *testing.T) {
+	testColor := color.FgRed
+	testName := "COMX"
+
+	testStream := NewSerialStream(testName, testColor)
+
+	formattedString := FormatSerialName(testStream)
+
+	if formattedString != "[COMX]" {
+		t.Errorf("expected formatted string [COMX] but got %s", formattedString)
+	}
+}
+
+
+func TestFormatEndLine(t *testing.T) {
+	
+}
+
+
+
+func TestPrint(t *testing.T) {
+
+	if testing.Verbose() {
+
+		PrintSimple("Hello World!")
+
+
+		testColor := color.FgRed
+		testName := "COMX"
+		testColor2 := color.FgGreen
+		testName2 := "COMY"
+
+		testStream := NewSerialStream(testName, testColor)
+		testStream2 := NewSerialStream(testName2, testColor2)
+
+		PrintFormat("Hello World!", testStream)
+		PrintFormat("Hello World!", testStream2)
+	}
+}
