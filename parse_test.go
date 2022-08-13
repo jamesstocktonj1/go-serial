d2605e43649f90315aea8bed738d5d914b2dd4df
js3g20 <js3g20@soton.ac.uk>
2022-08-13 11:46:42 +0100

    Add Sub Tests

--- parse_test.go
+++ parse_test.go
@@ -8,56 +8,69 @@ func TestHandleCommandParsing(t *testing.T) {
 
 	testFlagSet := HandleCommandParsing()
 
-	//test default state
-	testFlagSet.Parse([]string{})
+	t.Run("Default", func(t *testing.T) {
 
-	if *port != "COMX" {
-		t.Errorf("default port value expected COMX but got %s", *port)
-	}
-	
-	if *baud != 9600 {
-		t.Errorf("default baud value expected 9600 but got %d", *baud)
-	}
+		//test default state
+		testFlagSet.Parse([]string{})
 
-	if *dataBits != 8 {
-		t.Errorf("default data bits expected 8 but got %d", *dataBits)
-	}
+		if *port != "COMX" {
+			t.Errorf("default port value expected COMX but got %s", *port)
+		}
+		
+		if *baud != 9600 {
+			t.Errorf("default baud value expected 9600 but got %d", *baud)
+		}
 
-	if *stopBits != 1 {
-		t.Errorf("default stop bits expected 1 but got %d", *stopBits)
-	}
+		if *dataBits != 8 {
+			t.Errorf("default data bits expected 8 but got %d", *dataBits)
+		}
 
-	if *parity != "None" {
-		t.Errorf("default parity expected None but got %s", *parity)
-	}
+		if *stopBits != 1 {
+			t.Errorf("default stop bits expected 1 but got %d", *stopBits)
+		}
 
-	if *verbose != false {
-		t.Errorf("default verbose value expected false but got %t", *verbose)
-	}
+		if *parity != "None" {
+			t.Errorf("default parity expected None but got %s", *parity)
+		}
+
+		if *verbose != false {
+			t.Errorf("default verbose value expected false but got %t", *verbose)
+		}
+	})
 
 
-	testFlagSet.Parse([]string{"-com", "COM5"})
-	if *port != "COM5" {
-		t.Errorf("expected port value COM5 but got %s", *port)
-	}
+	t.Run("PortTest", func(t *testing.T) {
+		testFlagSet.Parse([]string{"-com", "COM5"})
+		if *port != "COM5" {
+			t.Errorf("expected port value COM5 but got %s", *port)
+		}
+	})
 
-	testFlagSet.Parse([]string{"-baud", "115200"})
-	if *baud != 115200 {
-		t.Errorf("expected baud value 115200 but got %d", *baud)
-	}
+	t.Run("BaudTest", func(t *testing.T) {
+		testFlagSet.Parse([]string{"-baud", "115200"})
+		if *baud != 115200 {
+			t.Errorf("expected baud value 115200 but got %d", *baud)
+		}
+	})
 
-	testFlagSet.Parse([]string{"-data", "9"})
-	if *dataBits != 9 {
-		t.Errorf("expected data bits value 9 but got %d", *dataBits)
-	}
+	t.Run("DataTest", func(t *testing.T) {
+		testFlagSet.Parse([]string{"-data", "9"})
+		if *dataBits != 9 {
+			t.Errorf("expected data bits value 9 but got %d", *dataBits)
+		}
+	})
 
-	testFlagSet.Parse([]string{"-stop", "2"})
-	if *stopBits != 2 {
-		t.Errorf("expected stop bits value 2 but got %d", *stopBits)
-	}
+	t.Run("StopTest", func(t *testing.T) {
+		testFlagSet.Parse([]string{"-stop", "2"})
+		if *stopBits != 2 {
+			t.Errorf("expected stop bits value 2 but got %d", *stopBits)
+		}
+	})
 
-	testFlagSet.Parse([]string{"-parity", "Odd"})
-	if *parity != "Odd" {
-		t.Errorf("expected parity Odd but got %s", *parity)
-	}
+	t.Run("ParityTest", func(t *testing.T) {
+		testFlagSet.Parse([]string{"-parity", "Odd"})
+		if *parity != "Odd" {
+			t.Errorf("expected parity Odd but got %s", *parity)
+		}
+	})
 }
