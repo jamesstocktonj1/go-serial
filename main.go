9396ace4bd9b81cef60afb5c11874e43bf949d05
js3g20 <js3g20@soton.ac.uk>
2022-08-14 08:56:14 +0100

    Move Serial Open

--- main.go
+++ main.go
@@ -11,41 +11,7 @@ func main() {
 	flagSet := HandleCommandParsing()
 	flagSet.Parse(os.Args[1:])
 
-	if *listPorts {
-		FormatListSerialPort()
-		os.Exit(3)
-	}
-	
-	if *defaultPort {
-		portsList := ListSerialPorts()
-
-		if len(portsList) == 0 {
-			fmt.Println("Error: No serial devices connected")
-			os.Exit(1)
-		} else {
-			*port = portsList[0]
-		}
-
-	} else {
-
-		if len(flagSet.Args()) == 0 {
-			fmt.Println("Error: No serial device specified")
-			os.Exit(1)
-		}
-		
-		*port = flagSet.Args()[0]
-
-		if !ParseComPort() {
-			fmt.Print("Error: Invalid serial device, device should take form ")
-
-			if os.Geteuid() == -1 {
-				fmt.Println("COMx")
-			} else {
-				fmt.Println("/dev/tty")
-			}
-			os.Exit(1)
-		}
-	}
+	OpenSerialPort(flagSet)
 
 	fmt.Printf("Serial Port: %s\n", *port)
 }
